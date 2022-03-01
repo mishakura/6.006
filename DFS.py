9ee01629f4921d05085096d046b4c9d95fc3f4bd
mishakura <michaelsojo22@gmail.com>
2022-03-01 16:17:52 -0300

    Full DFS

--- DFS.py
+++ DFS.py
@@ -1,15 +1,26 @@
-   #Adj = {0:  [4]
-#       1 : [5,2]
-#       2 : [5,1,3]
-#       3 : [4,2]
-#       4 : [0,3,5]
-#       5 : [1,2]}
+def dfs(Adj, s, parent = None, order = None):
+    if parent == None:
+        parent = [None for vertice in Adj] 
+        parent[s] = s
+        order = []
+    for vertice in Adj[s]:
+        if parent[vertice] is None:
+            parent[vertice] = s
+            dfs(Adj, vertice, parent, order)
+    order.append(s)
+    return parent, order
 
-parent = {s: None} #[0: None, 4: 0, 3 : 4, 2:]
-def DFS(Adj, s):
-    for v in Adj[s]:
-        if v not in parent:
-            parent[v] = s
-            DFS(Adj,v)
+def full_dfs(Adj):
+    parent = [None for vertice in Adj]
+    order = []
+    for vertice in range(len(Adj)):
+        if parent[vertice] is None:
+            parent[vertice] = vertice
+            dfs(Adj, vertice, parent, order)
+    return parent,order
 
+Adj = {0:[1], 1:[2], 2:[], 3:[4], 4:[]}
 
+        
+            
+        
